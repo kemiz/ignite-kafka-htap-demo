4d4d77356cebbc50d37c536e974672a78c4864da
kembot <kembot>
2018-09-08 19:13:36 +0100

    Added simple class that queries users and their stake

--- src/main/java/query/QueryIgnite.java
+++ src/main/java/query/QueryIgnite.java
@@ -1,4 +1,26 @@
 package query;
 
+import model.User;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import utils.IgniteConfigHelper;
+
 public class QueryIgnite {
+    public static void main(String[] args) throws InterruptedException {
+        IgniteConfiguration cfg = IgniteConfigHelper.getIgniteClientConfig();
+        Ignite ignite = Ignition.start(cfg);
+        IgniteCache<Integer, User> userCache = ignite.cache(IgniteConfigHelper.USER_CACHE);
+
+        while (true) {
+            for (int i = 0; i < userCache.size(); i++) {
+                User user = userCache.get(i);
+                System.out.println("name: " + user.getName() + ", total stake: " + user.getStake());
+            }
+            System.out.println("\n\n\n\n\n");
+            Thread.sleep(1000);
+        }
+
+    }
 }
