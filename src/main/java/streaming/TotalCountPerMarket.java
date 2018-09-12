43399b15f43975f3b4f59a33b614bde1bf64cdc2
kembot <kembot>
2018-09-12 22:26:50 +0100

    rename

--- src/main/java/streaming/TotalCountPerMarket.java
+++ src/main/java/streaming/TotalCountPerMarket.java
@@ -3,6 +3,9 @@ package streaming;
 import com.google.gson.Gson;
 import dao.MarketDao;
 import model.Bet;
+import model.Market;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -14,13 +17,14 @@ import utils.IgniteConfigHelper;
 import java.util.ArrayList;
 import java.util.Properties;
 
-public class MarketsSummary {
+public class TotalCountPerMarket {
 
     private MarketDao marketDao;
 
-    public MarketsSummary(){
+    public TotalCountPerMarket(){
 
-        marketDao = new MarketDao(Ignition.start(IgniteConfigHelper.getIgniteClientConfig()));
+//        marketDao = new MarketDao(Ignition.start(IgniteConfigHelper.getIgniteClientConfig()));
+
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "market-summary");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
@@ -28,35 +32,37 @@ public class MarketsSummary {
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.PRODUCER_PREFIX + ProducerConfig.INTERCEPTOR_CLASSES_CONFIG,
                 "io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor");
-
         props.put(StreamsConfig.CONSUMER_PREFIX + ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG,
                 "io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor");
 
-
         final StreamsBuilder builder = new StreamsBuilder();
+
         KStream<String, String> betStream = builder.stream("Bets");
+        KTable<String, String> marketTable = builder.table("Markets");
+
+        Gson g = new Gson();
 
         KStream<String, String> marketStream = betStream.flatMap((KeyValueMapper<String, String, Iterable<KeyValue<String, String>>>) (key, value) -> {
             ArrayList<KeyValue<String, String>> marketList = new ArrayList<>();
             try {
-                Bet bet = new Gson().fromJson(value, Bet.class);
-                System.out.println("received: " + bet);
-                ArrayList<Long> markets = bet.getMarkets();
-                System.out.println("  -- extracting market selection data");
-                for (int i = 0; i < markets.size(); i++) {
-                    marketList.add(new KeyValue<>(markets.get(i).toString(), marketDao.getMarketById(i).getMarket()));
-                }
+                ArrayList<String> markets = g.fromJson(value, Bet.class).getMarkets();
+                for (int i = 0; i < markets.size(); i++) marketList.add(new KeyValue<>(markets.get(i), markets.get(i)));
                 return marketList;
             } catch (Exception e) {
-                marketList.add(new KeyValue<>("0", "0"));
+                marketList.add(new KeyValue<>("0", "null"));
                 return marketList;
             }
         });
 
-        marketStream.to("MarketsSummary", Produced.with(Serdes.String(), Serdes.String()));
-
-        KStream<String, Long> marketCountStream = marketStream.groupByKey().count().toStream();
-        marketCountStream.to("MarketsCount", Produced.with(Serdes.String(), Serdes.Long()));
+        marketStream
+                /** group by key, count and create a new stream **/
+                .groupByKey()
+                .count()
+                .toStream()
+                /** join the market ids to the markets table and publish to new topic **/
+                .join(marketTable, (value1, value2) -> "Market: " + g.fromJson(value2, Market.class).getMarket() + ", Count: " + value1)
+                .through("Bet-Market-Selection-Feed")
+                .peek((key, value) -> System.out.println(value));
 
         final Topology topology = builder.build();
         System.out.println(topology.describe());
@@ -66,8 +72,7 @@ public class MarketsSummary {
     }
 
     public static void main(String[] args) {
-        MarketsSummary marketsSummary = new MarketsSummary();
-
+        TotalCountPerMarket totalCountPerMarket = new TotalCountPerMarket();
     }
 
 }
